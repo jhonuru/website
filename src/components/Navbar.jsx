bed389b6571956882824acc5ac08fdd7912a189d
Jhon Jairo Urueta Alvarez <jhon.urueta@misena.edu.co>
2024-01-15 23:18:17 -0500

    Modo oscuro

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faReact } from '@fortawesome/free-brands-svg-icons'
 
 export function Navbar() {
-  return(
+
+  function modeDark() {
+    var element = document.body;
+    element.dataset.bsTheme =
+      element.dataset.bsTheme == "light" ? "dark" : "light"
+  }
+
+  return (
     <>
-      <nav className="navbar bg-dark navbar-expand-md bg-body-tertiary" data-bs-theme="dark">
+      <nav className="navbar bg-dark navbar-expand-md bg-body-tertiary">
         <div className="container-fluid">
           <Link className="navbar-brand" to={"/website"}>
-            <FontAwesomeIcon icon={faReact} size='2x' style={{ color:'#00D1F7'}} className="ani1" />
+            <FontAwesomeIcon icon={faReact} size='2x' style={{ color: '#00D1F7' }} className="ani1" />
           </Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -34,6 +41,13 @@ export function Navbar() {
                 <a className="nav-link" href="#contacto">Contacto</a>
               </li>
             </ul>
+          </div>
+          <div className="row">
+            <div className="col-auto d-flex align-items-center">
+              <FontAwesomeIcon icon="fa-solid fa-sun" />
+              <div className="form-check form-switch mx-2 d-flex justify-content-center"><input className="form-check-input p-2" type="checkbox" role="switch" id="flexSwitchCheckCheked" onClick={modeDark} /></div>
+              <FontAwesomeIcon icon="fa-solid fa-moon" />
+            </div>
           </div>
         </div>
       </nav>
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 //Componentes
-import { ButtonTop } from '../components/ButtonTop'
-import { ButtonWhatsapp } from '../components/ButtonWhatsapp'
-import { Navbar } from '../components/Navbar'
-import { Home } from '../pages/Home'
+import { ButtonTop } from "../components/ButtonTop"
+import { ButtonWhatsapp } from "../components/ButtonWhatsapp"
+import { Navbar } from "../components/Navbar"
+import { Home } from "../pages/Home"
 import { NotFound } from "../pages/NotFound"
 
 export default function MyRoutes() {
